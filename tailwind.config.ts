6999829c7792ce7f8c9d99c7dcf97146d30c1183
severinlandolt <sev.landolt@gmail.com>
2024-05-27 22:54:42 +0200

    fix: replace safelist with template file path

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,8 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  safelist: ["stroke-amber-500", "bg-amber-500"],
   theme: {
     extend: {
       keyframes: {
