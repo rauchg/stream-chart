a8a3ed5cb42359a917eb511dc6c17279e752d3ab
severinlandolt <sev.landolt@gmail.com>
2024-05-27 22:43:36 +0200

    fix: replace safelist with content file path

--- app/chart.tsx
+++ app/chart.tsx
@@ -18,12 +18,6 @@ export function Chart({
         `$${Intl.NumberFormat("us").format(number).toString()}`
       }
       colors={
-        /* noteworthy: for this color to work, since it's not referenced
-         * directly in the code as a `className` and signal to the
-         * Tailwind compiler that it's used and to be included in the
-         * "pruned" css, we defined `stroke-amber - 500` and `bg - amber - 500`
-         * in the `safelist` prop of `tawildinw.config.js`
-         * https://sdk.vercel.ai/s/mjiuFU7cQtRN */
         ["amber"]
       }
       xAxisLabel="Month"
